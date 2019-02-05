25c01bbf15a1968a1af63e7f94fdc1c0d65ad746
Robin Homberg <robin.homberg@flagbit.de>
2019-02-05 12:35:07 +0100

    [ASG-1044] Further code cleanup

--- Adapter/S3Manager.php
+++ Adapter/S3Manager.php
@@ -2,30 +2,39 @@
 namespace Flagbit\FlysystemS3\Adapter;
  
 use \Magento\Framework\ObjectManagerInterface;
-use \Flagbit\Flysystem\Adapter\FilesystemManager;
 
 use \League\Flysystem\AwsS3v3\AwsS3Adapter;
 use \League\Flysystem\AwsS3v3\AwsS3AdapterFactory;
-use \Aws\S3\S3Client;
+use \Aws\S3\S3ClientFactory;
 
  
 /**
  * Class S3Manager
  * @package Flagbit\FlysystemS3\Adapter
  */
-class S3Manager extends FilesystemManager
+class S3Manager implements S3ManagerInterface
 {
 
+    /**
+     * @var AwsS3AdapterFactory
+     */
     protected $awsS3AdapterFactory;
+
+    /**
+     * @var S3ClientFactory
+     */
+    protected $s3ClientFactory;
 
     /**
      * S3Manager constructor
      * @param ObjectManagerInterface $objectManager
      */
     public function __construct(
-        AwsS3AdapterFactory $awsS3AdapterFactory
+        AwsS3AdapterFactory $awsS3AdapterFactory,
+        S3ClientFactory $s3ClientFactory
     ) {
         $this->awsS3AdapterFactory = $awsS3AdapterFactory;
+        $this->s3ClientFactory = $s3ClientFactory;
     }
 
     /**
@@ -39,7 +48,7 @@ class S3Manager extends FilesystemManager
      */
     public function createS3Driver(array $s3config, string $bucket, string $prefix = '', array $options = []): AwsS3Adapter
     {
-        $s3client = S3Client::factory($s3config);
+        $s3client = $this->s3ClientFactory->create(['args' => $s3config]);
 
         return $this->awsS3AdapterFactory->create(
             [
--- Test/Unit/Adapter/S3ManagerTest.php
+++ Test/Unit/Adapter/S3ManagerTest.php
@@ -7,6 +7,7 @@ use \Flagbit\FlysystemS3\Adapter\S3Manager;
 use \League\Flysystem\AwsS3v3\AwsS3Adapter;
 use \League\Flysystem\AwsS3v3\AwsS3AdapterFactory;
 use \Aws\S3\S3Client;
+use \Aws\S3\S3ClientFactory;
 
 class FilesystemManagerTest extends TestCase
 {
@@ -19,6 +20,16 @@ class FilesystemManagerTest extends TestCase
      * @var AwsS3Adapter|MockObject
      */
     protected $_awsS3AdapterMock;
+
+    /**
+     * @var S3ClientFactory|MockObject
+     */
+    protected $_s3ClientFactoryMock;
+
+    /**
+     * @var S3Client|MockObject
+     */
+    protected $_s3ClientMock;
 
     /**
      * @var S3Manager
@@ -36,8 +47,18 @@ class FilesystemManagerTest extends TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
+        $this->_s3ClientFactoryMock = $this->getMockBuilder(S3ClientFactory::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['create'])
+            ->getMock();
+
+        $this->_s3ClientMock = $this->getMockBuilder(S3Client::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
         $this->_object = new S3Manager(
-            $this->_awsS3AdapterFactoryMock
+            $this->_awsS3AdapterFactoryMock,
+            $this->_s3ClientFactoryMock
         );
     }
 
@@ -54,12 +75,15 @@ class FilesystemManagerTest extends TestCase
         $bucket = 'test';
         $prefix = 'test';
 
-        $s3ClientResult = S3Client::factory($s3config);
+        $this->_s3ClientFactoryMock->expects($this->once())
+            ->method('create')
+            ->with(['args' => $s3config])
+            ->willReturn($this->_s3ClientMock);
 
         $this->_awsS3AdapterFactoryMock->expects($this->once())
             ->method('create')
             ->with([
-                'client' => $s3ClientResult,
+                'client' => $this->_s3ClientMock,
                 'bucket' => $bucket,
                 'prefix' => $prefix,
                 'options' => []
